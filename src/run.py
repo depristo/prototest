c489fad4c4a86f54d74324ec9848daae3f12e473
Mark DePristo <mdepristo@google.com>
2018-07-06 22:17:19 +0000

    Working version

--- src/run.py
+++ src/run.py
@@ -4,12 +4,20 @@ from __future__ import print_function
 
 if __name__ == '__main__':
   print('-' * 40)
-  print('-' * 40)
-  print('-' * 40)
   print('hello, this is the first line of run.py')
 
-  print('About to import range_pb...')
-  from src import range_pb2
-
-  print('Successfully imported range_pb2')
+  print('-' * 40)
+  print('about to import api_implementation from protos to get version...')
+  from google.protobuf.internal import api_implementation
+  print('api_implementation.Type() says it uses the {} backend'.format(api_implementation.Type()))
   
+  print('-' * 40)
+  print('About to import our custom range_pb...')
+  from src import range_pb2
+  print('Successfully imported range_pb2')
+  print('-' * 40)
+  
+  print('Creating our own range pb instance')
+  range_ = range_pb2.Range(reference_name='chr1', start=10, end=20)
+  print('range_ is:\n{}'.format(range_))
+  print('-' * 40)
